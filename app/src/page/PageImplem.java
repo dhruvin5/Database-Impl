e485fecb4f57c30fb4e7469a0aab95df67860f18
dhruvin5 <dhruvingandhi5@gmail.com>
2025-03-02 09:16:41 -0500

    added page skeleton

--- app/src/page/PageImplem.java
+++ app/src/page/PageImplem.java
@@ -1,3 +1,21 @@
 public class PageImplem extends Page{
-    
+    Row getRow(int rowId)
+    {
+
+    }
+
+    int insertRow(Row row)
+    {
+
+    }
+
+    boolean isFull()
+    {
+
+    }
+
+    int getPid()
+    {
+
+    }
 }
