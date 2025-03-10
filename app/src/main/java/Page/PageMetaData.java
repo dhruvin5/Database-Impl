c7b92aabc6084b89f9e0ee8af96dbaee3f36fd41
dhruvin5 <dhruvingandhi5@gmail.com>
2025-03-09 20:31:46 -0400

    added documentation

--- app/src/main/java/Page/PageMetaData.java
+++ app/src/main/java/Page/PageMetaData.java
@@ -10,26 +10,32 @@ public class PageMetaData {
         this.dirtyBit = false;
     }
 
+    // get the pincount
     public int getPinCount() {
         return pinCount;
     }
 
+    // return if dirty or not
     public boolean isDirty() {
         return dirtyBit;
     }
 
+    // set the pin count
     public void setPinCount(int pinCount) {
         this.pinCount = pinCount;
     }
 
+    // set the dirty bit
     public void setDirtyBit(boolean dirtyBit) {
         this.dirtyBit = dirtyBit;
     }
 
+    // increment the pin count
     public void incrementPinCount() {
         this.pinCount++;
     }
 
+    // decrement the pin count
     public void decrementPinCount() {
         if (this.pinCount > 0) {
             this.pinCount--;
