4c5c792117f4ddbd9927153c5a8b35c5026e9a86
dhruvin5 <dhruvingandhi5@gmail.com>
2025-03-31 16:49:54 -0400

    added getter setters

--- app/src/main/java/Bplus/Rid.java
+++ app/src/main/java/Bplus/Rid.java
@@ -8,5 +8,21 @@ public class Rid {
         this.pageId = pageId;
         this.slotId = slotId;
     }
+
+    public void setPageId(int pageId) {
+        this.pageId = pageId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public int getPageId() {
+        return pageId;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
 }
 
