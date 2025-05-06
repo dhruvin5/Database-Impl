2a0624e6aa5efab8b990619a43d874906aaa1469
shreyabirthare <shreyabirthare16@gmail.com>
2025-05-05 22:14:58 -0400

    Added BNL cost calculation logic

--- app/src/main/java/org/example/AnalyticalIO.java
+++ app/src/main/java/org/example/AnalyticalIO.java
@@ -35,7 +35,7 @@ public class AnalyticalIO {
 
     // return matching number of rows in range after scanning through all the data
     public static int getRangeSelectionSelectivity(String start, String end) {
-        String fileName = start + "_" + end + "_analytical_match.csv";
+        String fileName = "1000_" + start + "_" + end + "_analytical_match.csv";
         String path = "C:\\Users\\HP\\Desktop\\ms\\645\\lab1\\645-Lab-32966720340112693401883534060222\\app\\shreya_perf_op\\" + fileName;
         return readMatchCountFromCSV(path);
     }
@@ -65,15 +65,40 @@ public class AnalyticalIO {
         return materializedPages;
     }
     
+    public static int getBNL1Cost(int m, int n, int o, int ms, int B) {
+        int N = (B-4)/2;
+        int bs = (Config.PAGE_SIZE - 4) / 39;
+        double passes = Math.ceil(1 + Math.ceil(((double) ms / bs) / N));
+        return m + n + (int) passes * o;
+    }
+    
 
-    // Optional helper if you want to print total directly
-    public static void printBaseIOCosts() {
+    public static int getBNL1Selectivity(int ms, int ws) {
+        return (int) (((double) ms * ws) / Math.max(ms, ws));
+    }
+
+    public static int getBNL2Cost(int t, int p, int B) {
+        int N = (B - 4) / 2;
+        int r = (Config.PAGE_SIZE - 4)/49;
+        double passes = Math.ceil(((double) t / r) / N);
+        return (int) (passes * p);
+    }
+
+    public static int getTotalPlanCost(int bnl1Cost, int bnl2Cost){
+        return (int)(bnl1Cost + bnl2Cost);
+    }
+
+    public static void printCosts() {
         int moviePages = getBaseMovieIO();
         int peoplePages = getBasePeopleIO();
         int workPages = getBaseWorkedOnIO();
         int materializedRows = AnalyticalIO.getMaterializedSelectivity();
         int titleRangeRows = AnalyticalIO.getRangeSelectionSelectivity("Caa", "Cab");
         int materializedPages = getMaterializedIO(materializedRows);
+        int bnl1Cost = getBNL1Cost(moviePages, workPages, materializedPages, titleRangeRows, 1000);
+        int bnl1Selectivity = getBNL1Selectivity(titleRangeRows, materializedRows);
+        int bnl2Cost = getBNL2Cost(bnl1Selectivity, peoplePages, 1000);
+        int totalPlanCost = getTotalPlanCost(bnl1Cost, bnl2Cost);
 
         System.out.println("Movies Base IO Cost: " + moviePages);
         System.out.println("People Base IO Cost: " + peoplePages);
@@ -82,5 +107,10 @@ public class AnalyticalIO {
         System.out.println("Movies Selectivity: " + titleRangeRows);
         System.out.println("Materialized I/O Cost: " + materializedPages);
         System.out.println("Total IO Cost for Base and Materialized Tables: " + (moviePages+peoplePages+workPages+materializedPages));
+        System.out.println("Cost Upto First BNL Join: "+ bnl1Cost);
+        System.out.println("BNL1 Selectivity "+ bnl1Selectivity);
+        System.out.println("BNL2 Cost: "+ bnl2Cost);
+        System.out.println("Total Plan Cost: " + totalPlanCost);
+
     }
 }
